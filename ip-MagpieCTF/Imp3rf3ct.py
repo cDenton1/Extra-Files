6f45e015eb92fa48f9ed5df94dd915b9a6a0d5af
Denton <163344926+cDenton1@users.noreply.github.com>
2025-02-26 15:24:08 -0700

    Update Imp3rf3ct.py

--- ip-MagpieCTF/Imp3rf3ct.py
+++ ip-MagpieCTF/Imp3rf3ct.py
@@ -1,12 +1,10 @@
-from Crypto.Util.number import long_to_bytes
-import gmpy2
+from Crypto.Util.number import long_to_bytes    # handle converting large number back into og string
+import gmpy2                                    # handle math and large operations
 
-# Given values
+# provided c value, encrypted message using RSA
 c = 121097813989638138346058726004363325645055007528244635971148694227865847570370087018045267126514696616744216943938638911580999312232908265654986785392772317303161096833644079741186123067589808986604522528602623394273539018995365633
 
-# Compute cube root using gmpy2
-m = gmpy2.iroot(c, 3)[0]  # Cube root (integer part)
+m = gmpy2.iroot(c, 3)[0]        # compute cube root of c
 
-# Convert back to bytes
-flag = long_to_bytes(int(m))
-print(flag.decode())
+flag = long_to_bytes(int(m))    # convert back to readable bytes
+print(flag.decode())            # print out the plaintext (flag)
