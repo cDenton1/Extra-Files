f51fac42e9d41f3cb4154aff97abd9e9be8c7e35
Denton <163344926+cDenton1@users.noreply.github.com>
2025-02-28 15:45:45 -0700

    Create Grey-Area.py

---
+++ ip-MagpieCTF/Grey-Area.py
@@ -0,0 +1,15 @@
+from Crypto.Util.number import long_to_bytes, inverse
+
+p = 961538449797931913175965696759    # p value found through factoring n
+q = 967319996044712948532221324531    # q value found through factoring n
+e = 0x10001    # 65537
+
+c = 311087069445751882711513305144448403394079226106736563961190    # provided c value
+n = 930115369455374918279214292565370334148036289536095273895029    # provided n value
+# n could also just be p * q
+
+x = (p - 1) * (q - 1)    # compute x, Euler’s Totient Function, required for computing d
+d = inverse(e, x)        # compute modular inverse
+
+m = pow(c, d, n)    # decrypting ciphertext
+print(long_to_bytes(m).decode())    # converting m to bytes, and printing
