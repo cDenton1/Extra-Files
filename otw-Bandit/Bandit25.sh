01deebc00c609200b8f35075a6433b4924a02191
Denton <163344926+cDenton1@users.noreply.github.com>
2025-05-17 22:03:33 -0600

    Update Bandit25.sh

--- otw-Bandit/Bandit25.sh
+++ otw-Bandit/Bandit25.sh
@@ -3,6 +3,4 @@
 for i in {0000..9999}
 do 
         echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 $i >> /tmp/dentontest/poss.txt
-done
-
-cat /tmp/dentontest/poss.txt | nc localhost 30002 > /tmp/dentontest/res.txt
+done | nc localhost 30002 | grep -v "Wrong"
