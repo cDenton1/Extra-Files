816689371ecd124becd339b3b42e4971ee88ccfa
Denton <163344926+cDenton1@users.noreply.github.com>
2025-05-17 21:55:56 -0600

    Update Bandit25-a2.sh

--- otw-Bandit/Bandit25-a2.sh
+++ otw-Bandit/Bandit25-a2.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
+PASS="gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8"
+
 for i in {0000..3333}
 do
-        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 %i >> /tmp/dentontest/poss1.txt
-done
+        echo $PASS $i
+done | nc -w 10 localhost 30002
+
+printf "end phase 1\n"
 
 for i in {3334..6666}
 do
-        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 %i >> /tmp/dentontest/poss2.txt
-done
+        echo $PASS $i
+done | nc -w 10 localhost 30002
+
+printf "end phase 2\n"
 
 for i in {6667..9999}
 do
-        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 %i >> /tmp/dentontest/poss3.txt
-done
+        echo $PASS $i
+done | nc -w 10 localhost 30002
 
-cat poss1.txt | nc localhost 30002 | grep -v "Wrong" > res1.txt
-
-then
-
-cat poss2.txt | nc localhost 30002 | grep -v "Wrong" > res2.txt
-
-then
-
-cat poss3.txt | nc localhost 30002 | grep -v "Wrong" > res3.txt
+printf "end phase 3\n"
