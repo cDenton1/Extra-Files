6a3bcc33ab7598264c2c765d1e7bd343cfb22e5c
Denton <163344926+cDenton1@users.noreply.github.com>
2025-05-17 21:55:37 -0600

    Update Bandit25.sh

--- otw-Bandit/Bandit25.sh
+++ otw-Bandit/Bandit25.sh
@@ -2,7 +2,7 @@
 
 for i in {0000..9999}
 do 
-        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 %i >> /tmp/dentontest/poss.txt
+        echo gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8 $i >> /tmp/dentontest/poss.txt
 done
 
 cat /tmp/dentontest/poss.txt | nc localhost 30002 > /tmp/dentontest/res.txt
